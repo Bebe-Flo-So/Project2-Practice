a1a900f7ba8d0d726b283fee53b25f18d8d9b57c
Bebe-Flo-So <120537192+Bebe-Flo-So@users.noreply.github.com>
2023-07-18 14:05:37 -0400

    cleaned up boilerplate

--- server/server.js
+++ server/server.js
@@ -6,5 +6,8 @@ const app = express()
 
 //set up a route for API
 app.get('/api', (req, res) => {
-
+    res.json({'users': ['userOne', 'userTwo', 'UserThree'] })
 })
+
+//server to listen
+app.listen(5000, () => {console.log("server listening on port 5000") })
